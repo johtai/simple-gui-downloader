de6b2c92fc3bb9925ddfc552a59c6a69e1dcbf63
johtai <johtai@yandex.ru>
2022-08-06 21:37:56 +0300

    add comments

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ class MainWindow(QMainWindow):
         self.initUI()
 
         self.button.clicked.connect(self.download)
+        # set the folder to which the files should be downloaded
         self.outpath_button.clicked.connect(self.choose_outpath)
+        # set the folder with yt-dlp and ffmpeg
         self.libpath_button.clicked.connect(self.choose_libpath)
 
+    # set global variables
     def set_globall(self):
         user = os.environ.get("USERNAME")
         self.outpath = f"C:\\Users\\{user}\\Desktop\\"
@@ -21,14 +24,18 @@ class MainWindow(QMainWindow):
         self.res = "Best"
 
     def initUI(self):
+        # load most of widgets from file
         uic.loadUi('main.ui', self)
 
+        # set text to appropriate labels
         self.outpath_label.setText(self.outpath)
         self.libpath_label.setText(self.libpath)
 
+        # connect buttons with functions (signals)
         self.video_button.toggled.connect(self.set_mode)
         self.audio_button.toggled.connect(self.set_mode)
 
+        # make a group to radiobuttons
         self.number_group1 = QButtonGroup(self)
         self.number_group1.addButton(self.video_button)
         self.number_group1.addButton(self.audio_button)
@@ -49,21 +56,26 @@ class MainWindow(QMainWindow):
             self.mode = radioBtn.text()
 
     def download(self):
+        # get a url
         url = self.line.text()
         print(url)
 
+        # make libpath the current folder for script
         os.chdir(self.libpath)
 
+        # choose the mode and make a commant to execute
         if self.mode == "Audio":
             command = f'yt-dlp --audio-format "mp3" --extract-audio {url}'
         elif self.mode == "Video":
             command = f'yt-dlp -S "res:{self.quality_box.currentText()}" {url}'
 
+        # execute the command
         os.system(command)
         self.command_label.setText(command)
 
         get_files = os.listdir(self.libpath)
         
+        # move files from libpath to outpath
         for file in get_files:
             if file.endswith(".mp3") or file.endswith(".mp4") or file.endswith(".opus") or file.endswith(".webm") or file.endswith(".mkv"):
                 shutil.move(self.libpath + file, self.outpath + file)
